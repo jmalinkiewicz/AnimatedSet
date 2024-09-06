f5361e6ff92d05c6cc8ed0ad208adc162063f62b
Jakub Malinkiewicz <me@jmalinkiewicz.pl>
2024-09-06 14:55:01 +0200

    Initial Commit

---
+++ SetCardGame/SetCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetCardGameApp.swift
+//  SetCardGame
+//
+//  Created by Jakub Malinkiewicz on 06/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SetCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

8adae6ea4cad5ee447bea6fc3418603250801b85
Jakub Malinkiewicz <me@jmalinkiewicz.pl>
2024-09-07 16:45:02 +0200

    fix build crashing, make card content a struct view, move view extensions to view file

--- SetCardGame/SetCardGameView.swift
+++ SetCardGame/SetCardGameView.swift
@@ -15,10 +15,9 @@ struct SetCardGameView: View {
             LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))], content: {
                 ForEach(viewModel.cardsOnDisplay.indices, id: \.self) { index in
                     let card  = viewModel.cardsOnDisplay[index]
-                    let content = viewModel.cardContentFactory(card)
                     let isSelected = viewModel.selectedCards.contains(card)
                     
-                    CardView(card: card, content: content as! AnyView)
+                    CardView(card)
                         .onTapGesture {
                             viewModel.select(card)
                         }
@@ -40,17 +39,113 @@ struct SetCardGameView: View {
 
 struct CardView: View {
     let card: SetCardGame.Card
-    let content: AnyView
-    
+
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 14)
                 .foregroundColor(.white)
                 .shadow(radius: 2)
-            content
+            CardContent(card)
                 .padding()
         }
         .aspectRatio(2/3, contentMode: .fit)
+    }
+    
+    init(_ card: SetCardGame.Card) {
+        self.card = card
+    }
+}
+
+struct CardContent: View {
+    let card: SetCardGame.Card
+    
+    var base: AnyView {
+        switch card.shape {
+        case .oval:
+            return AnyView(ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                   .strokeBorder(lineWidth: 2)
+                   .aspectRatio(3/1, contentMode: .fit)
+                   .shapePainter(color: card.color, shade: .solid)
+                RoundedRectangle(cornerRadius: 10)
+                    .aspectRatio(3/1, contentMode: .fit)
+                    .padding(1)
+                    .shapePainter(color: card.color, shade: card.shade)
+            })
+        case .diamond:
+            return AnyView(ZStack {
+                Rectangle()
+                    .rotation(.degrees(45), anchor: .center)
+                    .strokeBorder(lineWidth: 2)
+                    .aspectRatio(1, contentMode: .fit)
+                    .shapePainter(color: card.color, shade: .solid)
+                Rectangle()
+                    .rotation(.degrees(45), anchor: .center)
+                    .aspectRatio(1, contentMode: .fit)
+                    .padding(1)
+                    .shapePainter(color: card.color, shade: card.shade)
+            })
+        case .squiggle:
+            return AnyView(ZStack {
+                Rectangle()
+                   .strokeBorder(lineWidth: 2)
+                   .aspectRatio(3/1, contentMode: .fit)
+                   .shapePainter(color: card.color, shade: .solid)
+                Rectangle()
+                    .aspectRatio(3/1, contentMode: .fit)
+                    .padding(1)
+                    .shapePainter(color: card.color, shade: card.shade)
+            })
+        }
+    }
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            ForEach(0..<card.numberOfShapes.rawValue, id: \.self) { _ in
+                base
+            }
+        }
+    }
+    
+    init(_ card: SetCardGame.Card) {
+        self.card = card
+    }
+}
+
+struct ShapePainter: ViewModifier {
+    let color: SetCardGame.Card.Color
+    let shade: SetCardGame.Card.Shade
+    
+    var bgColor: Color {
+        switch color {
+        case .red:
+            return .red
+        case .green:
+            return .green
+        case .purple:
+            return .purple
+        }
+    }
+    var bgOpacity: Double {
+        switch shade {
+        case .solid:
+            return 1
+        case .striped:
+            return 0.45
+        case .open:
+            return 0.05
+        }
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .foregroundStyle(bgColor.opacity(bgOpacity))
+    }
+}
+
+extension View {
+    func shapePainter(color: SetCardGame.Card.Color, shade: SetCardGame.Card.Shade) -> some View {
+        modifier(ShapePainter(color: color, shade: shade))
     }
 }
 
